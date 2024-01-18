e867f94ef51d7198e6d6866d429dc2f88dd5b490
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2024-01-18 22:36:18 +0900

    [ flutter ] #4.1 Practice

--- Mobile/Flutter/Nomadcoders/a03_stateful_widgets_practice/lib/main.dart
+++ Mobile/Flutter/Nomadcoders/a03_stateful_widgets_practice/lib/main.dart
@@ -1,18 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onAdd() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void onSub() {
+    setState(() {
+      counter--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "click Button",
+                style: TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              Text(
+                "$counter",
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: onAdd,
+                    icon: const Icon(
+                      Icons.add_box,
+                      size: 30,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: onSub,
+                    icon: const Icon(
+                      Icons.indeterminate_check_box,
+                      size: 30,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
