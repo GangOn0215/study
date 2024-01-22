d102e3a1444767e5bc37b0a7ca961273ba272177
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2024-01-22 22:48:11 +0900

    flutter # 4.4 widget Lifecycle

--- Mobile/Flutter/Nomadcoders/a03_build_context/lib/main.dart
+++ Mobile/Flutter/Nomadcoders/a03_build_context/lib/main.dart
@@ -4,8 +4,20 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    showTitle = !showTitle;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,22 +29,56 @@ class App extends StatelessWidget {
           ),
         ),
       ),
-      home: const Scaffold(
+      home: Scaffold(
         body: Center(
-          child: MyLargeTitle(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              showTitle ? const MyLargeTitle() : const Text(''),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(
+                  Icons.energy_savings_leaf_outlined,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
-  const MyLargeTitle({
-    super.key,
-  });
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({super.key});
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  // 최초 widget이 생성될 때 한번 초기화 (build 함수 이전에)
+  void initState() {
+    super.initState();
+
+    print('init State');
+  }
+
+  @override
+  // widget이 위젯 트리에서 제거 되기 전에 실행되는 함수
+  void dispose() {
+    super.dispose();
+
+    print('dispose widget');
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('build!');
+
     return Text(
       'My Large Title',
       style: TextStyle(
