6d9ce881435b7aed220a3867cc771f2dcbaf99d3
GangOn0215 <ekdldktnekf@gmail.com>
2024-07-25 14:01:43 +0900

    set program

--- application/mobile/flutter/a05_pomodoro_practice/lib/screens/home_screen.dart
+++ application/mobile/flutter/a05_pomodoro_practice/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,74 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final int totalRound = 4;
+  final int totalGoal = 4;
+  final int pomodoroDefaultSec = 10;
+  late int totalSec;
+  late Timer timer; // 타이머
+  late int min = 0;
+  late int sec = 0;
+
+  int currentRound = 0;
+  int currentGoal = 0;
+
+  bool isRunning = false;
+
+  // 시간 제어
+  void onTick(Timer timer) {
+    setState(() {
+      totalSec--;
+
+      setTimeMinSec();
+
+      if (currentRound == 0) {
+        currentRound = 1;
+      }
+
+      if (totalSec == 0) {
+        timer.cancel();
+
+        totalSec = pomodoroDefaultSec;
+        currentRound++;
+
+        isRunning = false;
+      }
+    });
+  }
+
+  // 타이머 시작 눌렀을 때
+  void onTimeStart() {
+    if (isRunning) {
+      return;
+    }
+
+    isRunning = true;
+
+    timer = Timer.periodic(
+      Duration(seconds: 1),
+      onTick,
+    );
+  }
+
+  void setTimeMinSec() {
+    setState(() {
+      min = totalSec ~/ 60;
+      sec = totalSec % 60;
+    });
+  }
+
+  String showMinSec() {
+    return '${min.toString().padLeft(2, '0')} : ${sec.toString().padLeft(2, '0')}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    totalSec = 10;
+    setTimeMinSec();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +89,7 @@ class _HomeScreenState extends State<HomeScreen> {
               alignment: Alignment.bottomCenter,
               color: Theme.of(context).scaffoldBackgroundColor,
               child: Text(
-                '25:00',
+                '${showMinSec()}',
                 style: TextStyle(
                   color: Theme.of(context).textTheme.headlineSmall?.color,
                   fontWeight: FontWeight.w600,
@@ -34,7 +104,7 @@ class _HomeScreenState extends State<HomeScreen> {
               alignment: Alignment.center,
               color: Theme.of(context).textTheme.headlineSmall?.color,
               child: IconButton(
-                onPressed: () {},
+                onPressed: onTimeStart,
                 icon: Icon(
                   Icons.play_circle_outline_rounded,
                   color: Theme.of(context).scaffoldBackgroundColor,
@@ -65,7 +135,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                       Text(
-                        '0/4',
+                        '$currentRound/$totalRound',
                         style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.w600,
@@ -86,7 +156,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                       Text(
-                        '0/4',
+                        '$currentGoal/$totalGoal',
                         style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.w600,
