ca31df925466735b1a14628ddd5a8f560bdbfbc3
GangOn0215 <ekdldktnekf@mgmail.com>
2025-07-26 22:14:54 +0900

    [ flutter ] header, body, footer 만들어 보고 class 따로 만들어서 starIconBadge 로 컴포넌트화, 하단 footer에 실제 휴대폰 앱 처럼 전화, 메세지, 주소록 아이콘 넣기

--- mobile/flutter/apple_coding/a00_start/lib/main.dart
+++ mobile/flutter/apple_coding/a00_start/lib/main.dart
@@ -9,12 +9,40 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(), // header
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [StarIconBadge()],
+        ), // body
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.call),
+              Icon(Icons.message),
+              Icon(Icons.contact_page_sharp),
+            ],
+          ),
+        ), // footer
       ),
+    );
+  }
+}
+
+class StarIconBadge extends StatelessWidget {
+  const StarIconBadge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: const BoxDecoration(
+        color: Colors.yellow,
+        shape: BoxShape.circle,
+      ),
+      child: const Icon(Icons.star),
     );
   }
 }
