0b27918092663c011ead6bd33230a64df4818dbe
GangOn0215 <ekdldktnekf@mgmail.com>
2025-07-27 00:45:47 +0900

    [flutter] box list 만들기, wrap으로 감싸서 flex-wrap 처럼 넘어가면 자동으로 그 다음 라인 넘어갈수있게 처리 해보기

--- mobile/flutter/apple_coding/a01_box_list/lib/main.dart
+++ mobile/flutter/apple_coding/a01_box_list/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:a01_box_list/widgets/alphabet_box.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,11 +10,40 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        // header
+        appBar: AppBar(
+          title: const Text('App', style: TextStyle(color: Colors.white60)),
+          backgroundColor: Colors.blue.shade400,
         ),
+        body: Wrap(
+          direction: Axis.horizontal, // 나열 방향
+          alignment: WrapAlignment.start, // 정렬 방식
+          crossAxisAlignment: WrapCrossAlignment.center, // 교차 축 정렬
+          spacing: 5, // 좌우 간격
+          runSpacing: 5, // 상하 간격
+          children: [
+            const AlphabetBox(color: Colors.lightBlue, letter: 'A'),
+            const AlphabetBox(color: Colors.lightGreen, letter: 'B'),
+            const AlphabetBox(color: Colors.pink, letter: 'C'),
+            const AlphabetBox(color: Colors.orange, letter: 'D'),
+            const AlphabetBox(color: Colors.brown, letter: 'E'),
+            const AlphabetBox(color: Colors.teal, letter: 'F'),
+            const AlphabetBox(color: Colors.indigo, letter: 'G'),
+            const AlphabetBox(color: Colors.purpleAccent, letter: 'H'),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.call),
+              Icon(Icons.message),
+              Icon(Icons.contact_page_sharp),
+            ],
+          ),
+        ), // footer
       ),
     );
   }
