a1df2e806b9e88392c0bec4933fb3fa1b5459ea5
GangOn0215 <ekdldktnekf@gmail.com>
2025-08-17 19:24:43 +0900

    [ flutter ] 하단 카드 만들고, 왼쪽 텍스트 넣기 작업

--- mobile/flutter/normard/a03_ui_challenge/lib/main.dart
+++ mobile/flutter/normard/a03_ui_challenge/lib/main.dart
@@ -48,7 +48,7 @@ class MainApp extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 120),
+              SizedBox(height: 60),
               Row(
                 children: [
                   Column(
@@ -88,6 +88,75 @@ class MainApp extends StatelessWidget {
                     fontColor: Colors.white,
                   ),
                 ],
+              ),
+              SizedBox(height: 60),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Wallets',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                      color: Colors.white.withValues(alpha: 0.7),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Euro',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              '6 428',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'EUR',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white.withValues(alpha: 0.6),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
