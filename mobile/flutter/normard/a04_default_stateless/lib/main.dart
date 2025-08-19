13c6ceef1ecc2e6d5a813e07650469baec7c2413
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2025-08-20 00:21:14 +0900

    [ flutter ] Tistory - setState Counter 예시

--- mobile/flutter/normard/a04_default_stateless/lib/main.dart
+++ mobile/flutter/normard/a04_default_stateless/lib/main.dart
@@ -12,10 +12,38 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  int counter = 0;
+
+  void add() {
+    setState(() {
+      counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Click',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w400),
+              ),
+              Text(
+                '$counter',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.w600),
+              ),
+              IconButton(
+                onPressed: add,
+                icon: Icon(Icons.add_box_rounded, size: 36),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
