5ed9f5794cc9ce46ab4d45efb99ee5b6584ca65a
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2025-08-19 23:54:07 +0900

    [ flutter ] statelessWidget > statefulWidget

--- mobile/flutter/normard/a04_default_stateless/lib/main.dart
+++ mobile/flutter/normard/a04_default_stateless/lib/main.dart
@@ -4,17 +4,18 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
