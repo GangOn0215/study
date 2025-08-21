206ac1d37ee36b624f9ee069fc9aa6703c50921c
GangOn0215 <ekdldktnekf@gmail.com>
2025-08-22 00:48:43 +0900

    [ flutter ] 클릭 게임 - 숫자 패드 ( + 버튼 이벤트 )

--- mobile/flutter/a01_click_game/lib/main.dart
+++ mobile/flutter/a01_click_game/lib/main.dart
@@ -1,16 +1,60 @@
+import 'package:a01_click_game/widget/number_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final int maxButton = 9;
+  late List<int> numbers = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (int i = 0; i < maxButton; i++) {
+      numbers.add(i + 1);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    const double outer = 16; // 전체 바깥 여백
+    const double gap = 10; // 칸과 칸 사이 간격
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: const [
+            Row(children: [Text('Click GAME')]),
+          ],
+          backgroundColor: Colors.lightBlue,
+        ),
+
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < 3; i++)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(3, (j) {
+                    final number = (i * 3) + j + 1;
+                    return NumberButton(number: number);
+                  }),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ mobile/flutter/a01_click_game/lib/widget/number_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class NumberButton extends StatelessWidget {
+  int number = 0;
+
+  NumberButton({super.key, required this.number});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue, // 버튼 배경색
+          foregroundColor: Colors.white, // 텍스트/아이콘 색
+          minimumSize: const Size(80, 80), // 버튼 크기
+          shape: RoundedRectangleBorder(
+            // 모양
+            borderRadius: BorderRadius.circular(8),
+            side: const BorderSide(color: Colors.black), // 테두리
+          ),
+          padding: EdgeInsets.zero, // 내부 패딩
+        ),
+        onPressed: () {
+          debugPrint("Button $number clicked");
+        },
+        child: Text(
+          '$number',
+          style: const TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
