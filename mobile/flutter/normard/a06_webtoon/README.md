ce2a656a700bbcfec30b245d5e7165d410f96ffc
company_cookie <ekdldktnekf@gmail.com>
2025-09-09 15:49:07 +0900

    [ flutter ] init nomadcoders webtoon
    니꼬 선생님의 웹툰 앱 강의

---
+++ mobile/flutter/normard/a06_webtoon/README.md
@@ -0,0 +1,3 @@
+# a06_webtoon
+
+A new Flutter project.
---
+++ mobile/flutter/normard/a06_webtoon/android/app/src/main/kotlin/com/example/a06_webtoon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.a06_webtoon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
