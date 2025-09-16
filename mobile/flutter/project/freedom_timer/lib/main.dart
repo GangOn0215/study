309c18deb273707e68df923411bad78113c00fd4
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2025-09-16 19:20:30 +0900

    [ flutter ] 퇴근냥이 앱
    1. main.dar t에 있는 routes 폴더 안에 router.dart 따로 만들어 작업
    2. main.dart 에서  routes 들을 다 빼놨으니 최적화 작업

--- mobile/flutter/project/freedom_timer/lib/main.dart
+++ mobile/flutter/project/freedom_timer/lib/main.dart
@@ -1,82 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:freedom_timer/common/footer_navigation.dart';
-import 'package:freedom_timer/screens/home_screen.dart';
-import 'package:freedom_timer/screens/splash_screen.dart';
-import 'package:freedom_timer/screens/timer_screen.dart';
-import 'package:go_router/go_router.dart';
-
-final _router = GoRouter(
-  initialLocation: '/splash',
-  routes: [
-    // ✅ Splash → Home 전용 (Fade + Scale)
-    GoRoute(
-      path: '/splash',
-      pageBuilder: (context, state) => CustomTransitionPage(
-        child: const SplashScreen(),
-        transitionsBuilder: (context, animation, _, child) {
-          final scale = Tween<double>(
-            begin: 0.9,
-            end: 1.0,
-          ).animate(CurvedAnimation(parent: animation, curve: Curves.easeOut));
-          return FadeTransition(
-            opacity: animation,
-            child: ScaleTransition(scale: scale, child: child),
-          );
-        },
-        transitionDuration: const Duration(milliseconds: 700),
-      ),
-    ),
-
-    // ✅ ShellRoute (Home / Timer)
-    ShellRoute(
-      builder: (context, state, child) {
-        return Scaffold(
-          body: child,
-          bottomNavigationBar: const FooterNavigation(),
-        );
-      },
-      routes: [
-        // Home (왼쪽 슬라이드)
-        GoRoute(
-          path: '/',
-          pageBuilder: (context, state) => CustomTransitionPage(
-            child: HomeScreen(),
-            transitionsBuilder: (context, animation, _, child) {
-              final offset =
-                  Tween<Offset>(
-                    begin: const Offset(-1.0, 0.0), // 왼쪽에서 살짝
-                    end: Offset.zero,
-                  ).animate(
-                    CurvedAnimation(parent: animation, curve: Curves.easeOut),
-                  );
-              return SlideTransition(position: offset, child: child);
-            },
-            transitionDuration: const Duration(milliseconds: 300),
-          ),
-        ),
-
-        // Timer (오른쪽 슬라이드)
-        GoRoute(
-          path: '/timer',
-          pageBuilder: (context, state) => CustomTransitionPage(
-            child: const TimerScreen(),
-            transitionsBuilder: (context, animation, _, child) {
-              final offset =
-                  Tween<Offset>(
-                    begin: const Offset(-1.0, 0.0), // 오른쪽에서 들어옴
-                    end: Offset.zero,
-                  ).animate(
-                    CurvedAnimation(parent: animation, curve: Curves.easeOut),
-                  );
-              return SlideTransition(position: offset, child: child);
-            },
-            transitionDuration: const Duration(milliseconds: 300),
-          ),
-        ),
-      ],
-    ),
-  ],
-);
+import 'routes/app_router.dart';
 
 void main() {
   runApp(const MainApp());
@@ -89,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      routerConfig: _router,
+      routerConfig: appRouter,
 
       // 전역으로 적용할 테마
       theme: ThemeData(
---
+++ mobile/flutter/project/freedom_timer/lib/routes/app_router.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../screens/home_screen.dart';
+import '../screens/splash_screen.dart';
+import '../screens/timer_screen.dart';
+import '../common/footer_navigation.dart';
+import 'app_routes.dart';
+
+CustomTransitionPage _getPage(Widget screen) {
+  return CustomTransitionPage(
+    child: screen,
+    transitionsBuilder: (context, animation, _, child) {
+      final offset = Tween<Offset>(
+        begin: const Offset(-1.0, 0.0),
+        end: Offset.zero,
+      ).animate(CurvedAnimation(parent: animation, curve: Curves.easeOut));
+      return SlideTransition(position: offset, child: child);
+    },
+    transitionDuration: const Duration(milliseconds: 300),
+  );
+}
+
+final appRouter = GoRouter(
+  initialLocation: AppRoutes.splash,
+  routes: [
+    GoRoute(
+      path: AppRoutes.splash,
+      pageBuilder: (context, state) => _getPage(SplashScreen()),
+    ),
+    ShellRoute(
+      builder: (context, state, child) {
+        return Scaffold(
+          body: child,
+          bottomNavigationBar: const FooterNavigation(),
+        );
+      },
+      routes: [
+        GoRoute(
+          path: AppRoutes.home,
+          pageBuilder: (context, state) => _getPage(HomeScreen()),
+        ),
+        GoRoute(
+          path: AppRoutes.timer,
+          pageBuilder: (context, state) => _getPage(TimerScreen()),
+        ),
+      ],
+    ),
+  ],
+);
