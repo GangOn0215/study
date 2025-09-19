3b099055adb84b2eb02884a9be47c3ba176fa38f
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2025-09-19 22:26:04 +0900

    [ flutter ] 퇴근 냥이
    1. account screen 쪽 상단 유저의 아이콘 + 이름 & 레벨 나오게 수정

--- mobile/flutter/project/freedom_timer/lib/screens/account_screen.dart
+++ mobile/flutter/project/freedom_timer/lib/screens/account_screen.dart
@@ -12,7 +12,64 @@ class _AccountScreenState extends State<AccountScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(automaticallyImplyLeading: false, title: Text('팡팡월드 🐱')),
-      body: Column(children: [Row()]),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                crossAxisAlignment:
+                    CrossAxisAlignment.center, // 아이콘과 Column 세로 정렬
+                children: [
+                  Container(
+                    width: 80,
+                    height: 80,
+                    decoration: const BoxDecoration(shape: BoxShape.circle),
+                    child: const Icon(
+                      Icons.account_circle_sharp,
+                      size: 84,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(width: 12), // 아이콘과 텍스트 사이 간격
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+                      mainAxisAlignment: MainAxisAlignment.center, // 세로 가운데 정렬
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "푸른색 맛 쿠키 ",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextSpan(
+                                text: "(dmsqlctnekf@gmail.com)",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey[700],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        const Text("Lv. 1"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
