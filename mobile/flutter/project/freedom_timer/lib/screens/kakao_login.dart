332dcb38b707b861d2fa3aa76fee9a2eff51f248
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2025-10-15 03:32:24 +0900

    [ flutter ] 냥이 월드
    1. 로그아웃 기능 추가

--- mobile/flutter/project/freedom_timer/lib/screens/kakao_login.dart
+++ mobile/flutter/project/freedom_timer/lib/screens/kakao_login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:freedom_timer/models/kakao_user.dart';
 import 'package:freedom_timer/utils/user_preferences.dart';
 import 'package:go_router/go_router.dart';
@@ -112,42 +113,71 @@ class _KakaoLoginState extends State<KakaoLogin> {
     await _initLogin();
   }
 
+  Future<void> _onKakaoLogout() async {
+    try {
+      UserPreferences.removeUser();
+
+      Fluttertoast.showToast(
+        msg: "로그아웃 완료",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    } catch (error) {
+      print(">>> ERROR >>>");
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _isLoading
-            ? const CircularProgressIndicator()
-            : Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    'assets/images/kakao_login_medium_wide.png',
-                    height: 50,
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: _onLoginButtonPressed,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFFEE500), // 카카오 노란색
-                      foregroundColor: Colors.black,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 32,
-                        vertical: 12,
-                      ),
-                    ),
-                    child: const Text('카카오로 로그인'),
-                  ),
-                  if (_loginFailed)
-                    const Padding(
-                      padding: EdgeInsets.only(top: 16),
-                      child: Text(
-                        '로그인에 실패했습니다. 다시 시도해주세요.',
-                        style: TextStyle(color: Colors.red),
-                      ),
-                    ),
-                ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/kakao_login_medium_wide.png',
+              height: 50,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _onLoginButtonPressed,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFEE500), // 카카오 노란색
+                foregroundColor: Colors.black,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 12,
+                ),
               ),
+              child: const Text('카카오로 로그인'),
+            ),
+            if (_loginFailed)
+              const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  '로그인에 실패 했습니다. 다시 시도해주세요.',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ElevatedButton(
+              onPressed: _onKakaoLogout,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFFE003B), // 카카오 노란색
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 12,
+                ),
+              ),
+              child: Text('카카오 로그아웃'),
+            ),
+          ],
+        ),
       ),
     );
   }
