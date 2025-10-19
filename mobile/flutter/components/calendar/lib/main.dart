86eecf1a46800d9db7d9f4e7d9b1a082e8a3d8ac
GangOn0215 <ekdldktnekf@gmail.com>
2025-10-19 18:51:55 +0900

    [ flutter ] TableCalendar - eventLoader
    1. 특정 날짜에 이벤트를 Map 형식으로 묶어서 _events 변수에 담기
    2. 특정 날짜 이벤트 조회해서, 데이터 볼 수 있게  _getEventForDay 함수 추가

--- mobile/flutter/components/calendar/lib/main.dart
+++ mobile/flutter/components/calendar/lib/main.dart
@@ -22,17 +22,33 @@ class _MainAppState extends State<MainApp> {
   // 포커스 된 날짜
   DateTime _focusedDay = DateTime.now();
 
+  // 선택된 날짜인지 아닌지 판단
   bool _selectedDayPredicate(day) {
     final isSelected = isSameDay(_selectedDay, day);
 
     return isSelected;
   }
 
+  // 날짜 선택시 호출되는 함수
   void _onDaySelected(selectedDay, focusedDay) {
     setState(() {
       _selectedDay = selectedDay; // 선택한 날짜
       _focusedDay = focusedDay; // 포커스된 날짜
     });
+  }
+
+  // 클래스 안에 데이터 저장
+  final Map<DateTime, List<String>> _events = {
+    // Datetime.now() 를 하면 2025-10-19 17:35:00.000 이 되어 비교가 안됨
+    DateTime.utc(2025, 10, 15): ['출근', '퇴근', '회의', '점심', '스터디', '운동', '운동'],
+    DateTime.utc(2025, 10, 16): ['지각'],
+  };
+
+  // 특정 날짜의 이벤트 가져오기 ( 형태는 아까 a와 같음 )
+  List<String> _getEventsForDay(DateTime day) {
+    final key = DateTime.utc(day.year, day.month, day.day);
+
+    return _events[key] ?? [];
   }
 
   @override
@@ -54,6 +70,14 @@ class _MainAppState extends State<MainApp> {
                   titleTextFormatter: (date, locale) =>
                       DateFormat.yMMMM('ko_KR').format(date),
                 ),
+                // rerender 하면서 내가 선택한 날짜를 강조
+                selectedDayPredicate: _selectedDayPredicate,
+                // 날짜 선택시 호출되는 함수 ( onTab 과 비슷 )
+                onDaySelected: _onDaySelected,
+                // > 위 특정 날짜 이벤트 가져오기 함수 연결
+                //    >> marker 표시가 자동으로 표시 됨
+                //    >> ( 2025-10-15일에 2개, 16일에 1개 )
+                eventLoader: _getEventsForDay,
                 selectedDayPredicate: _selectedDayPredicate,
                 onDaySelected: _onDaySelected,
               ),
