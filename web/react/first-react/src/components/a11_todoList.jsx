3bf9de684577170ba4c68828507ee91b630b8d5c
company_cookie <ekdldktnekf@gmail.com>
2026-02-19 18:01:33 +0900

    [ feat ✨ ] first-react : 투두 리스트 구현 (a11)

---
+++ web/react/first-react/src/components/a11_todoList.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { MdOutlineCheckBox } from "react-icons/md";
+import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
+import './a11_todoList.css';
+
+/**
+ * TODO : 정리해야 할 것
+ *      0. delete 함수 완성
+ *      1. ES6 >> Array.prototype.map & Array.prototype.filter, Spread
+ *      2. React onClick 에서  onClick={() => {onUpdateTodos(id)} 으로 넣는것과 onClick={onUpdateTodos(id)} 로 넣는 것 차이 정리
+ *      3. [ Library ] React Icons 정리
+ */
+
+function A11TodoList() {
+    const [lastIndex, setLastIndex] = useState(1);
+    const [todoTitle, setTodoTitle] = useState('');
+
+    const [todos, setTodos] = useState([]);
+
+    /**
+     * CRUD
+     */
+
+    // create
+    function onAddTodos() {
+        console.log(todos);
+
+        setTodos([...todos, { id: lastIndex, title: todoTitle, isComplete: false }]);
+        setLastIndex(lastIndex + 1);
+
+        setTodoTitle('');
+    }
+
+    // delete
+    function onDelTodos() {
+
+    }
+
+    // update
+    function onUpdateTodos(id) {
+        const newTodos = todos.map((todo) => {
+            return todo.id == id ? { ...todo, isComplete: !todo.isComplete } : todo
+        })
+
+        console.log(newTodos);
+
+        setTodos(newTodos);
+    }
+
+    // Read
+    return (
+        <div className="todo-container">
+            <p className="todo-input-row">
+                <input className="todo-input" type="text" value={todoTitle} onChange={(e) => setTodoTitle(e.target.value)} /> 
+                <button className="todo-add-btn" onClick={onAddTodos}>ADD</button>
+            </p>
+            <div className="todo-list">
+                {todos.map((todo) => (
+                    <p key={todo.id} className={`todo-item ${todo.isComplete ? 'completed' : ''}`}>
+                        <span>{todo.title}</span>
+                        <button className="default-button todo-check-btn" onClick={() => onUpdateTodos(todo.id)}> { todo.isComplete ? <MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank />} </button>
+                    </p>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default A11TodoList;
---
+++ web/react/first-react/src/components/todolist.js
@@ -0,0 +1,35 @@
+const a = [1, 2, 3]  // ?
+const b = [ ...a ] // ?
+const c = [ ...a, 4 ] // ?
+
+let todos = [
+    { id: 1, title: '공부', isComplete: false },
+    { id: 2, title: '운동', isComplete: true }
+]; //?
+
+todos = [ ...todos, { id: 3, title: '산책', isComplete: false }] //?
+
+let updated = todos.map(todo =>
+    todo.id == 1 ? { ...todo, isComplete: true } : todo
+) // ?
+ 
+
+console.log(updated); // ?
+
+let deleted = todos.filter((todo) => {
+    return todo.id != 1;
+})
+
+console.log(deleted);
+
+const aa = [1, 2, 3];
+
+aa.filter((item) => {
+    return item > 1 ;
+}) // ?
+
+const aMap = [ 1, 2, 3 ];
+
+const uupdateAmap = aMap.map((item) => {
+    return item ;
+}) // ?
