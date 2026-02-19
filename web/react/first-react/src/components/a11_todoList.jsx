f8309070e83c875b65337b676f55555b6e09dec3
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2026-02-20 00:07:35 +0900

    [ refactor ♻️ ] frst-react: 탭 인덱스 수정

--- web/react/first-react/src/components/a11_todoList.jsx
+++ web/react/first-react/src/components/a11_todoList.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { MdOutlineCheckBox } from "react-icons/md";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 import { HiOutlineTrash } from "react-icons/hi";
-import './a11_todoList.css';
+import "./a11_todoList.css";
 
 /**
- * TODO : 
+ * TODO :
  *      0. ☑ delete 함수 완성
  *      1. ES6 >> Array.prototype.map & Array.prototype.filter, Spread 정리
  *      2. React onClick 에서  onClick={() => {onUpdateTodos(id)} 으로 넣는것과 onClick={onUpdateTodos(id)} 로 넣는 것 차이 정리
@@ -13,61 +13,89 @@ import './a11_todoList.css';
  */
 
 function A11TodoList() {
-    const [lastIndex, setLastIndex] = useState(1);
-    const [todoTitle, setTodoTitle] = useState('');
+  const [lastIndex, setLastIndex] = useState(1);
+  const [todoTitle, setTodoTitle] = useState("");
 
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    /**
-     * CRUD
-     */
+  /**
+   * CRUD
+   */
 
-    // create
-    function onAddTodos() {
-        setTodos([...todos, { id: lastIndex, title: todoTitle, isComplete: false }]);
-        setLastIndex(lastIndex + 1);
+  // create
+  function onAddTodos() {
+    setTodos([
+      ...todos,
+      { id: lastIndex, title: todoTitle, isComplete: false },
+    ]);
+    setLastIndex(lastIndex + 1);
 
-        setTodoTitle('');
-    }
+    setTodoTitle("");
+  }
 
-    // delete
-    function onDelTodos(id) {
-        const newTodos = todos.filter((todo) => {
-            return todo.id !== id;
-        });
+  // delete
+  function onDelTodos(id) {
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
 
-        setTodos(newTodos);
-    }
+    setTodos(newTodos);
+  }
 
-    // update
-    function onUpdateTodos(id) {
-        const newTodos = todos.map((todo) => {
-            return todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
-        })
+  // update
+  function onUpdateTodos(id) {
+    const newTodos = todos.map((todo) => {
+      return todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo;
+    });
 
-        setTodos(newTodos);
-    }
+    setTodos(newTodos);
+  }
 
-    // Read
-    return (
-        <div className="todo-container">
-            <p className="todo-input-row">
-                <input className="todo-input" type="text" value={todoTitle} onChange={(e) => setTodoTitle(e.target.value)} /> 
-                <button className="todo-add-btn" onClick={onAddTodos}>ADD</button>
-            </p>
-            <div className="todo-list">
-                {todos.map((todo) => (
-                    <p key={todo.id} className={`todo-item ${todo.isComplete ? 'completed' : ''}`}>
-                        <span>{todo.title}</span>
-                        <div className="todo-button-wrap">
-                            <button className="default-button todo-check-btn" onClick={() => onUpdateTodos(todo.id)}> { todo.isComplete ? <MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank />} </button>
-                            <button className="default-button todo-trash-btn" onClick={() => onDelTodos(todo.id)}><HiOutlineTrash /></button>
-                        </div>
-                    </p>
-                ))}
+  // Read
+  return (
+    <div className="todo-container">
+      <p className="todo-input-row">
+        <input
+          className="todo-input"
+          type="text"
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+        />
+        <button className="todo-add-btn" onClick={onAddTodos}>
+          ADD
+        </button>
+      </p>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <p
+            key={todo.id}
+            className={`todo-item ${todo.isComplete ? "completed" : ""}`}
+          >
+            <span>{todo.title}</span>
+            <div className="todo-button-wrap">
+              <button
+                className="default-button todo-check-btn"
+                onClick={() => onUpdateTodos(todo.id)}
+              >
+                {" "}
+                {todo.isComplete ? (
+                  <MdOutlineCheckBox />
+                ) : (
+                  <MdOutlineCheckBoxOutlineBlank />
+                )}{" "}
+              </button>
+              <button
+                className="default-button todo-trash-btn"
+                onClick={() => onDelTodos(todo.id)}
+              >
+                <HiOutlineTrash />
+              </button>
             </div>
-        </div>
-    )
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default A11TodoList;
